c0936345c56f4b8fb0155b2ded3460a35381aa5c
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-03-06 03:56:48 +0600

    data added on firebase done

---
+++ app/src/main/java/io/github/treebricks/uubdc/Models/Donation.kt
@@ -0,0 +1,8 @@
+package io.github.treebricks.uubdc.Models
+
+import java.util.*
+
+/**
+ * Created by fahim on 6/3/18.
+ */
+data class Donation(val quantity: Int, val donationDate: String)
--- app/src/main/java/io/github/treebricks/uubdc/RegistrationActivity.kt
+++ app/src/main/java/io/github/treebricks/uubdc/RegistrationActivity.kt
@@ -3,12 +3,25 @@ package io.github.treebricks.uubdc
 import android.app.DatePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.NonNull
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_registration.*
-import kotlinx.android.synthetic.main.activity_search.*
 import java.text.SimpleDateFormat
 import java.util.*
+import com.google.firebase.firestore.FirebaseFirestore
+import io.github.treebricks.uubdc.Models.Donation
+import io.github.treebricks.uubdc.Models.Donor
+import java.time.LocalDate
+import kotlin.collections.HashMap
+
 
 class RegistrationActivity : AppCompatActivity() {
+
+    // Access a Cloud Firestore instance from your Activity
+    var db = FirebaseFirestore.getInstance()
+
+    val TAG = RegistrationActivity::javaClass.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +56,49 @@ class RegistrationActivity : AppCompatActivity() {
                     myCalendar.get(Calendar.MONTH),
                     myCalendar.get(Calendar.DAY_OF_MONTH))
                     .show()
+        }
+
+        bRegister.setOnClickListener { _ ->
+            val donorName = etDonorName.text.toString()
+            val donorEmail = etDonorEmail.text.toString()
+            val donorMobile = etMobileNo.text.toString()
+            val bloodGroup = sBloodGroup.selectedItem.toString()
+            val area = etArea.text.toString()
+            val lastDonationDate = etLastDonationDate.text.toString()
+
+            var validationFlag = true
+
+            if(donorName.isEmpty()){
+                etDonorName.error = "Donor name is required"
+                validationFlag = false
+            }
+            if(donorMobile.isEmpty()){
+                etMobileNo.error = "Mobile no is required"
+                validationFlag = false
+            }
+            if(area.isEmpty()) {
+                etArea.error = "Area is required"
+                validationFlag = false
+            }
+
+            if(validationFlag)
+            {
+                val newDonor = Donor(donorName, donorEmail, donorMobile, bloodGroup, area,
+                        lastDonationDate, emptyList())
+
+//                val donorMap = HashMap<String, Donor>()
+//                donorMap.put("dono")
+
+                db.collection("donors")
+                        .add(newDonor)
+                        .addOnSuccessListener { documentReference ->
+                            Log.d(TAG, "Document Snapshot add with ID: " + documentReference.id)
+                            Toast.makeText(this@RegistrationActivity, "Donor registration successfully!", Toast.LENGTH_SHORT).show()
+                        }.addOnFailureListener { exception ->
+                            Log.w(TAG, "Error adding Document: ", exception)
+                            Toast.makeText(this@RegistrationActivity, "Donor registration failed!", Toast.LENGTH_SHORT).show()
+                        }
+            }
         }
 
     }
