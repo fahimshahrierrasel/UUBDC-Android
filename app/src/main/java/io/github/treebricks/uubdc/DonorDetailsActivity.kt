5f5b8de62928666732337674bf7d459896dbadd1
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-03-16 20:26:01 +0600

    Call and Sms service add

--- app/src/main/java/io/github/treebricks/uubdc/DonorDetailsActivity.kt
+++ app/src/main/java/io/github/treebricks/uubdc/DonorDetailsActivity.kt
@@ -1,25 +1,47 @@
 package io.github.treebricks.uubdc
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import com.afollestad.materialdialogs.MaterialDialog
 import com.google.firebase.firestore.FirebaseFirestore
+import com.ramotion.circlemenu.CircleMenuView
 import io.github.treebricks.uubdc.Models.Donor
 import kotlinx.android.synthetic.main.activity_donor_details.*
+import kotlinx.android.synthetic.main.content_donor_details.*
+import android.support.v4.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.widget.Toast
+import android.preference.PreferenceManager
+import com.afollestad.materialdialogs.MaterialDialog
+import android.text.InputType
+
+
 
 
 class DonorDetailsActivity : AppCompatActivity() {
 
-    var donorId: String? = null
+    private var donorId: String? = null
     private val mDocRef = FirebaseFirestore.getInstance()
-    val donorsRef = mDocRef.collection("donors")
+    private val donorsRef = mDocRef.collection("donors")
+    private var donor: Donor? = null
+    private var message: String = " "
+
+    companion object {
+        val CALL_REQUEST_CODE: Int = 1
+        val SMS_REQUEST_CODE: Int = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donor_details)
-        setSupportActionBar(toolbarDonDetails)
+        setSupportActionBar(toolbarDonorDetails)
+        toolbar_layout.isTitleEnabled = false
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
         donorId = if(savedInstanceState == null) {
             intent.extras?.getString("DONOR_ID")
         }
@@ -27,13 +49,14 @@ class DonorDetailsActivity : AppCompatActivity() {
             savedInstanceState.getSerializable("DONOR_ID") as String
         }
 
+
         val query = donorsRef.whereEqualTo("id", donorId)
 
         query.get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 for (document in task.result) {
 
-                    val donor = Donor(
+                    donor = Donor(
                             Id = document.get("id").toString(),
                             DonorName = document.get("donorName").toString(),
                             MobileNo = document.get("mobileNo").toString(),
@@ -44,20 +67,85 @@ class DonorDetailsActivity : AppCompatActivity() {
                             TotalDonation = document.get("totalDonation").toString().toInt()
                     )
 
-                    tvDonorName.text = donor.DonorName
-                    tvBloodGroup.text = donor.BloodGroup
-                    tvMobileNo.text = donor.MobileNo
-                    tvArea.text = donor.Area
-                    tvEmail.text = donor.Email
-                    tvTotalDonation.text = donor.TotalDonation.toString();
-                    tvLastDonationDate.text = donor.LastDonationDate
-
+                    tvDonorName.text = getString(R.string.donor_name_plc, donor?.DonorName)
+                    tvBloodGroup.text = getString(R.string.blood_group_plc, donor?.BloodGroup)
+                    tvMobileNo.text = getString(R.string.mobile_no_plc,donor?.MobileNo)
+                    tvArea.text = getString(R.string.area_plc,donor?.Area)
+                    tvEmail.text = getString(R.string.email_plc,donor?.Email)
+                    tvTotalDonation.text = getString(R.string.total_donation_plc,donor?.TotalDonation.toString())
+                    tvLastDonationDate.text = getString(R.string.last_donation_date_plc,donor?.LastDonationDate)
                 }
+                message = "Hello, ${donor?.DonorName}. Are you well?"
             }
         }
 
+        circle_menu.eventListener=object : CircleMenuView.EventListener(){
+            override fun onButtonClickAnimationEnd(view: CircleMenuView, buttonIndex: Int) {
+                when(buttonIndex)
+                {
+                    0 -> {
+                        MaterialDialog.Builder(this@DonorDetailsActivity)
+                                .title("New Donation Date")
+                                .content("New donation date for the donor. (dd-mm-yyyy)")
+                                .inputType(InputType.TYPE_CLASS_DATETIME)
+                                .input("Donation Date", "", { dialog, input ->
+                                    // Do something
+                                }).show()
+                    }
+                    // Call
+                    1 -> {
+                        val callIntent = Intent(Intent.ACTION_CALL)
+                        callIntent.data = Uri.parse("tel:${donor?.MobileNo}")
+
+                        if (ActivityCompat.checkSelfPermission(this@DonorDetailsActivity, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(this@DonorDetailsActivity, arrayOf(android.Manifest.permission.CALL_PHONE), CALL_REQUEST_CODE)
+                        } else {
+                            startActivity(callIntent)
+                        }
+                    }
+                    //SMS
+                    2 -> {
+                        val smsIntent = Intent(Intent.ACTION_VIEW)
+                        smsIntent.data = Uri.parse("sms:${donor?.MobileNo}")
+                        smsIntent.putExtra("sms_body", message)
+                        if (ActivityCompat.checkSelfPermission(this@DonorDetailsActivity, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(this@DonorDetailsActivity, arrayOf(android.Manifest.permission.SEND_SMS), SMS_REQUEST_CODE)
+                        } else {
+                            startActivity(smsIntent)
+                        }
+                    }
+                }
+            }
+        }
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            when (requestCode) {
+            //Call
+                1 -> {
+                    val callIntent = Intent(Intent.ACTION_CALL)
+                    callIntent.data = Uri.parse("tel:${donor?.MobileNo}")
+                    if (ActivityCompat.checkSelfPermission(this@DonorDetailsActivity, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        startActivity(callIntent)
+                    }
+                }
+            //SMS
+                2 -> {
+                    val smsIntent = Intent(Intent.ACTION_VIEW)
+                    smsIntent.data = Uri.parse("sms:${donor?.MobileNo}")
+                    smsIntent.putExtra("sms_body", message)
+                    if (ActivityCompat.checkSelfPermission(this@DonorDetailsActivity, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                        startActivity(smsIntent)
+                    }
+                }
+            }
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_donor_details, menu)
